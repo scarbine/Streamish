3ebe94e30ab0b39e6c8ea0478bf4e8bc29a4798b
Sam Carbine <scarbine@me.com>
2021-10-07 12:01:13 -0500

    adding header to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import ApplicationViews from "./components/ApplicationViews";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <ApplicationViews />
+        <Header />
+        <ApplicationViews />
       </Router>
     </div>
   );
